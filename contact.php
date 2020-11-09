ae40d60d53a1265f985ea66dc653f6dd9978eb7b
Pascal Raabe <50845335+PascalRaabe@users.noreply.github.com>
2020-11-09 12:38:37 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,37 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Contact</title>
+		<charset="UTF-8">
+	</head>
+	<body>	
+		
+		<ul>
+			<h1>CONTACT</h1>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="https://www.instagram.com/pascal_raabe/">Visit my Insta</a></li>
+			<li><a href="https://www.instagram.com/juliaswierczyna/">Visit my girlfriend's Insta!</a></li>
+		</ul>
+
+		<form>
+			<input="text" name="first" placeholder="Firstname">
+			<br>
+			<input="text" name="last" placeholder="Lastname">
+			<br>
+			<input="text" name="e-mail" placeholder="E-mail">
+			<br>
+			<input="text" name="uid" placeholder="Username">
+			<br>
+			<input="password" name="pwd" placeholder="Password">
+			<br>
+			<button type="submit" name="submit">Sign Up</button>
+			<br>
+		</form>
+		<?php
+					
+		?>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+	include 'style3.css';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+</head>
+	<body>
+		<section class="sec1"></section>
+		<section class="sec2"></section>
+		<section class="sec3"></section>
+		<section class="sec4"></section>
+		<section class="sec5"></section>
+		<section class="sec6"></section>
+		<section class="sec7"></section>
+		<section class="sec8"></section>
+		<section class="sec9"></section>
+		<section class="sec10"></section>
+		<section class="sec11"></section>
+		
+		<section class="sec12"></section>
+		<section class="sec13"></section>
+		<section class="sec14"></section>
+		<section class="sec15"></section>
+		<section class="sec16"></section>
+		<section class="sec17"></section>
+	</body>
+</html>
